f012c79aecfafa7b1bef7051feaa5c06766f63b4
Felipe Olavarria <178.felipe@gmail.com>
2022-05-26 15:22:59 -0400

    import usePillBox in MedicinesList

--- components/medicinePillsList/index.tsx
+++ components/medicinePillsList/index.tsx
@@ -2,26 +2,40 @@ import {
   Box,
   VStack,
   Text,
+  Spinner
 } from "@chakra-ui/react";
+import usePillBox from "../../hooks/usePillBox";
 import MedicinePills from "../../model/MedicinePills";
 import Pills from "./Pills";
 
-const MedicinesList = () => (
-  <VStack align={'left'}>
-    <Box bgColor="#F5F5F5" py={2}>
-      <Text 
-        mx={'1rem'} 
-        fontSize={"19px"} 
-        lineHeight={"28px"} 
-        letterSpacing={"0.15px"}>
-        Te queda
-      </Text>
-    </Box>
-    <VStack w={'full'}>
-      {dummyData.map(item => <Pills key={item.name} medicinePills={item} />)}
+const MedicinesList = () => {
+  const { pills, isLoading, isError } = usePillBox();
+
+  if (isLoading) return <Spinner />
+  if (isError) return (
+    <Text color={'red.500'}>
+      Hubo un error, por lo que no se pudieron cargar los datos.
+    </Text>
+  )
+
+  return (
+    <VStack align={'left'}>
+      <Box bgColor="#F5F5F5" py={2}>
+        <Text 
+          mx={'1rem'} 
+          fontSize={"19px"} 
+          lineHeight={"28px"} 
+          letterSpacing={"0.15px"}>
+          Te queda
+        </Text>
+      </Box>
+      <VStack w={'full'}>
+        {pills != undefined && 
+          pills.map(item => <Pills key={item.name} medicinePills={item} />)}
+      </VStack>
     </VStack>
-  </VStack>
-);
+  );
+}
 
 const dummyData: MedicinePills[] = [
   {
