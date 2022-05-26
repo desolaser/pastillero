a203768d8eda92acd3ad942d97c817ef143a13d6
Felipe Olavarria <178.felipe@gmail.com>
2022-05-26 12:06:10 -0400

    Add textStyles for Pills component

--- styles/theme.ts
+++ styles/theme.ts
@@ -5,6 +5,32 @@ const theme = extendTheme({
     heading: `'Poppins', sans-serif`,
     body: `'Poppins', sans-serif`,
   },
+  textStyles: {
+    pillName: {
+      fontSize: ["15px", "20px"],
+      fontWeight: "semibold",
+      lineHeight: "150%",
+      letterSpacing: "0.15px",
+    },
+    pillMg: {
+      fontSize: ["13px", "18px"],
+      fontWeight: "normal",
+      lineHeight: "150%",
+      letterSpacing: "0.15px",
+    },
+    remainingPills: {
+      fontSize: ["13px", "18px"],
+      fontWeight: "normal",
+      lineHeight: "150%",
+      letterSpacing: "0.15px",
+    },
+    remainingDays: {
+      fontSize: ["12px", "17px"],
+      fontWeight: "normal",
+      lineHeight: "150%",
+      letterSpacing: "0.04px",
+    },
+  },
 });
 
 export default theme;
