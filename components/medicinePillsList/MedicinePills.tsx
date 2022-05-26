565dfb1266114066b388e30efb021afd854cc98f
Felipe Olavarria <178.felipe@gmail.com>
2022-05-26 10:25:28 -0400

    Create MedicinePills component

---
+++ components/medicinePillsList/MedicinePills.tsx
@@ -0,0 +1,38 @@
+import {
+  HStack,
+  VStack,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import MedicinePills from "../../model/MedicinePills";
+
+type MedicinePillsProps = {
+  medicinePills: MedicinePills
+};
+
+const MedicinePills = ({ medicinePills }: MedicinePillsProps) => {
+  return (
+    <HStack>
+      <Image />
+      <VStack>
+        <Image
+          boxSize='50px'
+          objectFit='cover'
+          src={medicinePills.image}
+          alt='Dan Abramov'
+        />
+        <Text>
+          {medicinePills.name}
+        </Text>
+        <Text>
+          {medicinePills.mg}          
+        </Text>
+        <Text>
+          {`Quedan ${medicinePills.remainingPills} píldoras para ${medicinePills.remainingDays} días.`}
+        </Text>
+      </VStack>
+    </HStack>
+  );
+}
+
+export default MedicinePills;
