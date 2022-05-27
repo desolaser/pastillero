f5128265c7028571ab76122a36ff553db25b7784
Desolaser <178.felipe@gmail.com>
2022-05-26 22:57:12 -0400

    Add Product model

---
+++ model/Product.ts
@@ -0,0 +1,11 @@
+type Product = {
+  id: number, 
+  name: string,
+  concentration: string, 
+  imagesUrl: string,
+  format: number,
+  price: number,
+  containerQuantity: number
+};
+
+export default Product;
