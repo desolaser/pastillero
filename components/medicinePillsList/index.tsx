239e505018420df8748fe230f49b1d8a02abb77e
Desolaser <178.felipe@gmail.com>
2022-05-26 23:19:53 -0400

    Remove dummyData and use MedicinePills to define item type in pills.map

--- components/medicinePillsList/index.tsx
+++ components/medicinePillsList/index.tsx
@@ -28,35 +28,11 @@ const MedicinesList = () => {
         </Text>
       </Box>
       <VStack w={'full'}>
-        {pills?.map(item => <Pills key={item.name} medicinePills={item} />)}
+        {pills?.map((item: MedicinePills) => 
+          <Pills key={item.name} medicinePills={item} />)}
       </VStack>
     </VStack>
   )
 }
-
-
-const dummyData: MedicinePills[] = [
-  {
-    image: 'https://bit.ly/dan-abramov', 
-    name: 'Paracetamol',
-    mg: '100 mg', 
-    remainingPills: 10,
-    remainingDays: 10
-  },
-  {
-    image: 'https://bit.ly/dan-abramov', 
-    name: 'Ibuprofeno',
-    mg: '100 mg', 
-    remainingPills: 20,
-    remainingDays: 20
-  },
-  {
-    image: 'https://bit.ly/dan-abramov', 
-    name: 'Tapsin',
-    mg: '100 mg', 
-    remainingPills: 30,
-    remainingDays: 30
-  },
-];
 
 export default MedicinesList;
