e2c3c57d197242fa46a61f84ee1bfd34e5e472c1
Felipe Olavarria <178.felipe@gmail.com>
2022-05-27 11:15:37 -0400

    Change cartButton to be an actual button

--- components/cartButton/index.tsx
+++ components/cartButton/index.tsx
@@ -1,10 +1,10 @@
-import { Box } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import PlusIcon from "./PlusIcon";
 import CartIcon from "./CartIcon";
 
 const CartButton = () => {
   return (
-    <Box style={{ position: "relative" }}>
+    <Button style={{ position: "relative" }} variant="link">
       <CartIcon w={12} h={12} p={0} m={0} />
       <PlusIcon 
         style={{
@@ -13,7 +13,7 @@ const CartButton = () => {
           bottom: "12px"
         }}
         w={12} h={12} p={0} m={0} />
-    </Box>
+    </Button>
   )
 };
 
