4418cec95ac755bd3adda80f3cc72e02272157a9
Felipe Olavarria <178.felipe@gmail.com>
2022-05-27 11:21:40 -0400

    Restrucutre usePillbox to never return undefined values

--- hooks/usePillBox.ts
+++ hooks/usePillBox.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MedicinePills from "../model/MedicinePills";
 import useProducts from "../hooks/useProducts";
 import usePurchases from "../hooks/usePurchases";
@@ -5,64 +6,87 @@ import PurchaseDetail from "../model/PurchaseDetail";
 import Purchase from "../model/Purchase";
 
 type PillBoxValues = {
-  pills: MedicinePills[] | void | undefined,
+  pills: MedicinePills[],
   isLoading: boolean,
   isError: boolean
 }
 
-const usePillBox = (userId: number) => {
+const usePillBox = () => {
+  const [ pills, setPills ] = useState<MedicinePills[]>([]);
   const { products, isError: isErrorProd, isLoading: isLoadingProd } = useProducts();
   const { purchases, isError: isErrorPurchases, isLoading: isLoadingPurchases } = usePurchases();
 
-  const discountedPurchases = purchases?.map((purchase: Purchase) => {
+  useEffect(() => {
+    if (products && purchases) {
+      const pillsData = getPills();
+      setPills(pillsData);
+    }
+  }, [products, purchases]);
+
+  const getDateDifference = (date: Date) => {
     const actualDate: Date = new Date();
-    const receivedDate: Date = new Date(purchase.received_date);
+    const receivedDate: Date = new Date(date);
     const diffTime = Math.abs(actualDate.getTime() - receivedDate.getTime());
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-    const discountedPills = diffDays;
-    const newPurchaseDetails = purchase.details.map(detail => {
-      const newQuantity = detail.quantity - discountedPills
+    return diffDays;
+  }
 
-      return ({      
-        ...detail, 
-        quantity: newQuantity > 0 ? newQuantity : 0
+  const getPills = () => {
+    const discountedPurchases = purchases?.map((purchase: Purchase) => {
+      const discountedPills = getDateDifference(purchase.received_date);
+      const newPurchaseDetails = purchase.details.map(detail => {
+        const newQuantity = detail.quantity - discountedPills
+        purchase.purchase_id
+        return ({      
+          ...detail, 
+          quantity: newQuantity > 0 ? newQuantity : 0
+        })
       })
-    })
-
-    return {
-      ...purchase,
-      details: newPurchaseDetails
-    }
-  });
-
-  const purchasesDetails = discountedPurchases?.reduce((arr: PurchaseDetail[], purchase: Purchase) => {
-    return [...arr, ...purchase.details]
-  }, []);
-
-  const inventory = purchasesDetails?.reduce((obj: any, value) => {
-    const key: string = value.product_id.toString();
-    if (key in obj) {
-      obj[key] += value.quantity;
-    } else {
-      obj[key] = value.quantity;
-    }
-
-    return obj;
-  }, {});
-
-  const pills = inventory && Object.keys(inventory).map(itemKey => {
-    const product = products?.find(product => product.id == parseInt(itemKey));
-    if (product) {
-      return {        
-        image: product.imagesUrl, 
-        name: product.name,
-        mg: product.concentration, 
+  
+      return {
+        ...purchase,
+        details: newPurchaseDetails
+      }
+    });
+  
+    const purchasesDetails = discountedPurchases?.reduce((arr: PurchaseDetail[], purchase: Purchase) => {
+      return [...arr, ...purchase.details]
+    }, []);
+  
+    const inventory = purchasesDetails?.reduce((obj: any, value) => {
+      const key: string = value.product_id.toString();
+      if (key in obj) {
+        obj[key] += value.quantity;
+      } else {
+        obj[key] = value.quantity;
+      }
+  
+      return obj;
+    }, {});
+  
+    const pills: MedicinePills[] = inventory ? Object.keys(inventory).map(itemKey => {
+      const product = products?.find(product => product.id == parseInt(itemKey));
+      if (product) {
+        return {
+          image: product.imagesUrl,
+          name: product.name,
+          mg: product.concentration,
+          remainingPills: inventory[itemKey],
+          remainingDays: inventory[itemKey],
+        }
+      }
+      return {
+        image: "",
+        name: "Producto no encontrado",
+        mg: "",
         remainingPills: inventory[itemKey],
         remainingDays: inventory[itemKey],
-      }
-    }
-  });
+      };
+    }) : [];
+
+    return pills;
+  }
 
   return {
     pills,
